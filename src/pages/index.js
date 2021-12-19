5174ff4a48a1f853e57d2e97562d218a736ff945
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-19 13:50:05 -0800

    add my picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
-//import { StaticImage } from "../images/pic.jpg"
+import StaticImage from "../images/pic.jpg"
+import Astronaut from "../images/gatsby-astronaut.png"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -13,20 +14,30 @@ const IndexPage = () => (
       <br/>― Neil deGrasse Tyson</p>
     <p>“Never doubt that a small group of thoughtful, committed, citizens can change the world. Indeed, it is the only thing that ever has.”
       <br/> ― Margaret Mead</p>
-    {/* <StaticImage
+     {/* <StaticImage
       src={StaticImage}
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
       alt="Cas Spicer, Software Developer"
       style={{ marginBottom: `1.45rem` }}
-    /> */}
-    <p>
+    />   */}
+    <div
+    style={{
+      backgroundImage: `url(${StaticImage})`,
+      backgroundSize: `cover`,
+      height: `20em`,
+      width: `15em`,
+      border: `5px solid red`
+  }}
+
+    ></div>
+    {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    </p> */}
   </Layout>
 )
 
