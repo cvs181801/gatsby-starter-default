e680764087c271bf572313fb08aa4f1d16557855
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-20 14:44:31 -0800

    add sticky position to header

--- src/components/header.js
+++ src/components/header.js
@@ -11,37 +11,41 @@ const Header = ({ siteTitle }) => (
     }}
   >
     <div
-      id="headerContain"
+      className="headerContain"
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`
       }}
     >
-      <div class="h1Container">  
-            <h1 style={{ margin: 0 }}>
-              <Link
-                to="/"
-                style={{
-                  color: `white`,
-                  textDecoration: `none`,
-                }}
-              >
-                {siteTitle}
-              </Link>
-            </h1>
-      </div>
-      <div class="navContainer">
-        <nav>
-          <ul class="navUl">
-            <li class="navLi"><a class="navLiAnchor" href="#headerContain">home</a></li>
-            <li class="navLi"><a class="navLiAnchor" href="#aboutmeSection">about me</a></li>
-            <li class="navLi"><a class="navLiAnchor" href="#skillsSection">skills</a></li>
-            <li class="navLi"><a class="navLiAnchor" href="#projectsSection">projects</a></li>
-            <li class="navLi"><a class="navLiAnchor" href="#contactSection">contact</a></li>
-          </ul>
-        </nav>
-      </div>
+     {/* <section
+      id="headerSection"
+     > */}
+          <div class="h1Container">  
+                <h1 style={{ margin: 0 }}>
+                  <Link
+                    to="/"
+                    style={{
+                      color: `white`,
+                      textDecoration: `none`,
+                    }}
+                  >
+                    {siteTitle}
+                  </Link>
+                </h1>
+          </div>
+          <div class="navContainer">
+            <nav>
+              <ul class="navUl">
+                <li class="navLi"><a class="navLiAnchor" href="#headerSection">home</a></li>
+                <li class="navLi"><a class="navLiAnchor" href="#aboutmeSection">about me</a></li>
+                <li class="navLi"><a class="navLiAnchor" href="#skillsSection">skills</a></li>
+                <li class="navLi"><a class="navLiAnchor" href="#projectsSection">projects</a></li>
+                <li class="navLi"><a class="navLiAnchor" href="#contactSection">contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        {/* </section> */}
     </div>
   </header>
 )
