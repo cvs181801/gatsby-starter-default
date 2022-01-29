b114262c46626903b319c7bef3b0e1ca0c9acf54
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-01-29 11:45:12 -0800

    add content to project compopnent re: 15 Minutes app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Pic15Minutes from "../images/15Minutes.png"
 
 export default function Projects() {
     return (
@@ -8,14 +9,31 @@ export default function Projects() {
             >
             <h1
                 className= "sectionH1"
-            >Check back to view my projects soon.</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam ut venenatis tellus in metus vulputate. Lorem donec massa sapien faucibus et molestie ac. Felis donec et odio pellentesque diam volutpat commodo sed. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Netus et malesuada fames ac turpis egestas integer eget aliquet. Justo eget magna fermentum iaculis eu non diam phasellus. Tempus quam pellentesque nec nam aliquam sem. Nunc sed id semper risus. In est ante in nibh mauris cursus. Id semper risus in hendrerit gravida rutrum quisque. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Et netus et malesuada fames ac turpis egestas integer. Enim sed faucibus turpis in. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Sed sed risus pretium quam vulputate dignissim suspendisse in. Facilisis volutpat est velit egestas dui id. Et netus et malesuada fames ac turpis. Diam quam nulla porttitor massa id neque.
+            >Projects.</h1>
+            <h2>15 Minutes</h2>
+            <div
+                style={{
+                    backgroundImage: `url(${Pic15Minutes})`,
+                    backgroundSize: `cover`,
+                    height: `20em`,
+                    width: `15em`,
+                    margin: `1em 0 1em 0`
+                }}
+            ></div>
+                <p>
+                    15 Minutes was built using the Twitter API.  It's an app with a unique take on the concept introduced by Andy Warhol; "15 Minutes of Fame".  
 
-Eget est lorem ipsum dolor. Nunc congue nisi vitae suscipit tellus mauris a diam. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Sed viverra tellus in hac habitasse platea dictumst vestibulum. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Ac turpis egestas integer eget aliquet. In aliquam sem fringilla ut morbi. Tincidunt augue interdum velit euismod in pellentesque massa placerat. Vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing. Nulla facilisi etiam dignissim diam quis. Accumsan sit amet nulla facilisi morbi tempus iaculis. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Ultricies tristique nulla aliquet enim tortor at.
+                    This project was built in React and Node.js with Express, using the Twitter API.  
+                    Find more info on the Twitter API <a href="https://developer.twitter.com/en/products/twitter-api">here.</a>
 
-Mattis nunc sed blandit libero volutpat sed cras. Eget mi proin sed libero enim sed. Augue lacus viverra vitae congue eu. Pharetra sit amet aliquam id diam maecenas. Amet nisl purus in mollis nunc sed. Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Nisl nisi scelerisque eu ultrices vitae. Eu consequat ac felis donec. Risus viverra adipiscing at in tellus integer. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Auctor urna nunc id cursus metus aliquam eleifend mi in. Semper eget duis at tellus at urna.
+                    I think Andy would have loved all forms of social media, including Twitter.  He was fascinated by celebrities and the phenomenon of fame.
+                    The goal of this project is to encourage the user/viewer to think critically about their use of social media.
+                    Now that we're living in an age when each person can have their 15 minutes, isn't it time to take Andy's excitement a step further?  
+                    Instead of simply having your 15 minutes, shouldn't we use that 15 minutes for something positive? 
 
-Diam sit amet nisl suscipit adipiscing bibendum est ultricies. Arcu ac tortor dignissim convallis aenean et tortor. Convallis tellus id interdum velit. Neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Mattis aliquam faucibus purus in massa tempor nec. Id consectetur purus ut faucibus pulvinar elementum integer enim neque. Velit dignissim sodales ut eu sem integer vitae justo eget. Lorem mollis aliquam ut porttitor leo. Tellus in hac habitasse platea dictumst. Leo a diam sollicitudin tempor id. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Fusce ut placerat orci nulla pellentesque dignissim enim sit. In ante metus dictum at. Hac habitasse platea dictumst quisque sagittis purus. Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit.</p>
+                    I decided to showcase 5 inspirational celebrities who are frequently using their Twitter platform to send positive messages to their fans.
+                    <a href="https://calm-citadel-32694.herokuapp.com/">Check out the app here.</a>
+                </p>
         </section>
         </div>
     )
