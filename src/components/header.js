3e54cf586360b2de9fd59d35b4f4449551be84a7
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-05-25 10:33:18 -0700

    adjust the flexbox on nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import "./layout.css"
 
 const Header = ({ siteTitle }) => (
   <header
+    className="header"
     style={{
       background: `red`,
-      marginBottom: `1.45rem`,
-      height: `11em`
+      //marginBottom: `1.45rem`
     }}
   >
     <div
@@ -22,25 +23,44 @@ const Header = ({ siteTitle }) => (
       id="headerSection"
      > 
           <div className="h1Container">  
-                <h1 style={{ margin: 0 }}>
+                <h1 style={{ 
+                    margin: 0,
+                    //fontSize: `1.5rem`  // this will need to be removed , to be replaced with responsive inline styling.
+                    }}>
                   <Link
                     to="/"
                     style={{
                       color: `white`,
-                      textDecoration: `none`,
+                      textDecoration: `none`
                     }}
                   >
                     {siteTitle}
                   </Link>
                 </h1>
           </div>
-          <div className="navContainer">
+          <div 
+            className="navContainer"
+            style={{
+              margin: `0 auto`,
+              //border: `1px solid blue`,
+            }}
+
+          >
             <nav>
-              <ul className="navUl">
-                <li className="navLi"><a className="navLiAnchor" href="#aboutmeSection">about me</a></li>
-                <li className="navLi"><a className="navLiAnchor" href="#skillsSection">skills</a></li>
-                <li className="navLi"><a className="navLiAnchor" href="#projectsSection">projects</a></li>
-                <li className="navLi"><a className="navLiAnchor" href="#contactSection">contact</a></li>
+              <ul 
+                className="navUl"
+                //style={{border: `1px solid green`,}}
+              
+              >
+                <li className="navLi"><a className="navLiAnchor" href="#aboutmeSection" style={{flexWrap: `wrap`}}>about me</a></li>
+                <li className="navLi"><a className="navLiAnchor" href="#skillsSection" style={{flexWrap: `wrap`}}>skills</a></li>
+                </ul>
+                <ul
+                className="navUl"
+                //style={{border: `1px solid green`,}}
+                >
+                <li className="navLi"><a className="navLiAnchor" href="#projectsSection" style={{ flexWrap: `wrap`}}>projects</a></li>
+                <li className="navLi"><a className="navLiAnchor" href="#contactSection" style={{ flexWrap: `wrap`}}>contact</a></li>
               </ul>
             </nav>
           </div>
